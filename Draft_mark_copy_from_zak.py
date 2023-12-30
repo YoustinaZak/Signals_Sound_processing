ee9b4e21df1c43b65f7e1ed201b383c2f7402236
Mark Magdy <mark.anis12@gmail>
2023-12-31 01:12:13 +0200

    new functions from scipy

--- Draft_mark_copy_from_zak.py
+++ Draft_mark_copy_from_zak.py
@@ -2,11 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from scipy.signal import butter, filtfilt
+from scipy.fftpack import fft, fftfreq, ifft
 
-# Function to read audio file
-def read_audio_file(file_path):
-    sample_rate, audio_data = wavfile.read(file_path)
+
+# function of accessing (reading) the audio file
+def read_file(f_name):
+    sample_rate, audio_data = wavfile.read(f_name)
     return sample_rate, audio_data
+
 
 def convert_stereo_to_mono(audio_data):
     if audio_data.ndim > 1 and audio_data.shape[1] == 2:  # Check if audio is stereo
@@ -16,53 +19,49 @@ def convert_stereo_to_mono(audio_data):
         # If the audio is already mono or not in the expected shape, return the original data
         return audio_data
 
-file_path = "test.wav"
-sample_rate, audio = read_audio_file(file_path)
 
-# Convert stereo audio to mono (if it's stereo)
-audio = convert_stereo_to_mono(audio)
-
-# Plotting function for time domain representation
-def plot_time_domain(sample_rate, audio_data):
+# plot audio file (time domain)
+def plt_time_domain(sample_rate, audio_data, title):
     time = np.arange(0, len(audio_data)) / sample_rate
     plt.figure(figsize=(10, 6))
     plt.plot(time, audio_data)
-    plt.xlabel('Time (s)')
+    plt.xlabel('Time')
     plt.ylabel('Amplitude')
-    plt.title('Time Domain Representation')
+    plt.title(title)
     plt.grid()
     plt.show()
 
 
-plot_time_domain(sample_rate,audio)
-# Plotting function for frequency domain representation
-def plot_frequency_domain(positive_freq, magnitude):
+# plot audio file (freq domain)
+def plt_freq_domain(positive_freq, magnitude, title):
     plt.figure(figsize=(10, 6))
     plt.plot(positive_freq, magnitude)
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('Magnitude')
-    plt.title('Frequency Domain Representation')
+    plt.title(title)
     plt.grid()
     plt.show()
+    return
 
-# Function to perform Fourier Transform
+
+# function of using fourier transform
 def perform_fourier_transform(audio_data, sample_rate):
     fft_result = np.fft.fft(audio_data)
     frequencies = np.fft.fftfreq(len(fft_result), 1 / sample_rate)
     positive_freq = frequencies[:len(frequencies) // 2]
     magnitude = np.abs(fft_result)[:len(frequencies) // 2]
-    return positive_freq, magnitude
+    phase = np.angle(fft_result)[:len(frequencies) // 2]
+    return positive_freq, magnitude, phase
 
-positive_freq, magnitude = perform_fourier_transform(audio, sample_rate)
-plot_frequency_domain(positive_freq, magnitude)
 
-# Function for low-pass filtering
+# apply filter lowpass and high pass filter
 def low_pass_filter(cut_freq, order, sample_rate, audio_data):
     nyquist = 0.5 * sample_rate
     normal_cutoff = cut_freq / nyquist
     b, a = butter(order, normal_cutoff, btype='low', analog=False)
     filtered_audio = filtfilt(b, a, audio_data)
     return filtered_audio
+
 
 # Function for high-pass filtering
 def high_pass_filter(cut_freq, order, sample_rate, audio_data):
@@ -72,18 +71,10 @@ def high_pass_filter(cut_freq, order, sample_rate, audio_data):
     filtered_audio = filtfilt(b, a, audio_data)
     return filtered_audio
 
-# Applying high-pass filter
-audio = high_pass_filter(1000, 5, sample_rate, audio)
-positive_freq, magnitude = perform_fourier_transform(audio, sample_rate)
-plot_frequency_domain(positive_freq, magnitude)
-
-# Applying low-pass filter
-audio = low_pass_filter(5000, 5, sample_rate, audio)
-positive_freq, magnitude = perform_fourier_transform(audio, sample_rate)
-plot_frequency_domain(positive_freq, magnitude)
 
 def save_audio_to_file(file_path, sample_rate, audio_data):
-    wavfile.write(file_path, sample_rate, audio_data)
+    wavfile.write(file_path, sample_rate, np.asarray(audio_data, dtype=np.int16))
+
 
 def normalize_audio(audio_data):
     max_val = np.max(np.abs(audio_data))
@@ -94,23 +85,48 @@ def normalize_audio(audio_data):
     return audio_data_normalized
 
 
-def float_to_pcm16(audio_data):
-    # Clip the audio data to the range of [-1, 1)
-    audio_data = np.clip(audio_data, -1.0, 1.0 - np.finfo(np.float16).eps)
+def band_filter_in_frequency(magnitude, cuttoff_low, cuttoff_high):
+    smpls = len(magnitude)
+    mask = np.zeros(smpls)
+    mask[int((cuttoff_low / 24000) * smpls): int((cuttoff_high / 24000) * smpls)] = 1
+    magnitude = magnitude * mask
+    return magnitude
 
-    # Scale the data to the range of a 16-bit integer
-    pcm_data = np.int16(audio_data * 32767)
-    return pcm_data
+
+def preform_inverse_fourier_transform(magnitude, phase):
+    filtered = magnitude * np.exp(1j * phase)
+    time_domain_signal = np.fft.ifft(filtered)
+    return time_domain_signal
 
 
-# ... (Previous code remains unchanged)
+############################################################
 
-# Normalize audio data before saving
-audio_normalized = normalize_audio(audio)
+if __name__ == "__main__":
+    audioFile = "track.wav"
+    sampleRate, audioData = read_file(audioFile)
+    print(audioData[1000:2000])
+    # Convert stereo audio to mono if it's stereo
+    audioData = convert_stereo_to_mono(audioData)
 
-# Convert floating-point audio to PCM 16-bit integer format
-pcm_audio = float_to_pcm16(audio_normalized)
-plot_time_domain(sample_rate,pcm_audio)
-# Save the modified audio to a new file
-output_file_path = "Modified_Audio.wav"  # Change this to your desired output filename
-save_audio_to_file(output_file_path, sample_rate, pcm_audio)
+    plt_time_domain(sampleRate, audioData, 'Time Domain Representation Before editing')
+
+
+    magnitude = sp = fft(audioData)  ############# transformmmmm
+
+
+
+    magnitude = band_filter_in_frequency(magnitude, 580, 10000)
+
+
+
+    audioData = ifft(magnitude).real ########### inverseeeeeeeeeeeee
+
+
+
+    plt_time_domain(sampleRate, audioData, 'Time Domain Representation after band-pass filter')
+    #print(audioData[6000:2000])
+    # audioNormalized = normalize_audio(audioData)
+
+    # Save to a new file
+    output_file_path = "Modified_Audio.wav"  # Change this to your desired output filename
+    save_audio_to_file(output_file_path, sampleRate, audioData)
